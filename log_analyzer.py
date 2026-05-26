9aa9c29c66e3e88797c293c95dabce2cc784a71d
Kluuuz <sker877@gmail.com>
2026-05-26 23:06:31 +0800

    added file monitor

--- log_analyzer.py
+++ log_analyzer.py
@@ -1,7 +1,8 @@
+file = input("File to monitor:" )
 search = input("Search Query: ").lower()
 num = 0
 
-with open("logs/sample_logs.txt","r") as logs:
+with open(f"logs/{file}","r") as logs:
     for filter in logs:
         num += 1
         if search in filter.lower():
