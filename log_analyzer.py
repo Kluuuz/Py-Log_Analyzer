2928837654d33beff8e0ade783864e3d6fcd258e
Kluuuz <sker877@gmail.com>
2026-05-26 23:52:25 +0800

    Added match percentage and some tweaking

--- log_analyzer.py
+++ log_analyzer.py
@@ -6,17 +6,23 @@ print("=============================== \n")
 file = input("File to monitor: " )
 search = input("Filter log: ").lower()
 num = 0
+match = 0
 found = False
 
 with open(f"logs/{file}","r") as logs:
+    print("\nResults: ")
     for filtered in logs:
+        num += 1
         if search in filtered.lower():
-            num += 1
+            match += 1
             found = True
-            print(f"{num}. {filtered.strip()}")
+            print(f"{match}. {filtered.strip()}")
     if not found:   
          print("No matched ")
 
 #Summary
 print("\n===============================\n============SUMMARY============\n===============================\n")
-print(f"Number of {search.capitalize()}: {num}")
+print(f"Number of {search.capitalize()}: {match}")
+if num > 0 :
+    percent = (match / num) * 100
+    print(f"Match rate: {percent:.2f}%")
