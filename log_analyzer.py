e89ed4d9eb64fa35ecc0629df7d49978f7fd31f2
Kluuuz <sker877@gmail.com>
2026-05-26 23:11:06 +0800

    added a banner

--- log_analyzer.py
+++ log_analyzer.py
@@ -1,10 +1,15 @@
-file = input("File to monitor:" )
+print("===============================")
+print("NOTE: CREATE A 'logs' folder first")
+print("=============================== \n")
+
+
+file = input("File to monitor: " )
 search = input("Search Query: ").lower()
 num = 0
 
 with open(f"logs/{file}","r") as logs:
-    for filter in logs:
+    for filtered in logs:
         num += 1
-        if search in filter.lower():
-            print(f"{num}. {filter.strip()}")
+        if search in filtered.lower():
+            print(f"{num}. {filtered.strip()}")
 
