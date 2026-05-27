c4d4ec425765c6a8980271c33d7f578f051dbb9f
Kluuuz <sker877@gmail.com>
2026-05-27 22:22:14 +0800

    Added an IP counter feature

--- log_analyzer.py
+++ log_analyzer.py
@@ -1,3 +1,5 @@
+import re
+
 print("===============================")
 print("NOTE: CREATE A 'logs' folder first")
 print("=============================== \n")
@@ -9,6 +11,8 @@ num = 0
 match = 0
 found = False
 
+ip_num = {}
+
 with open(f"logs/{file}","r") as logs:
     print("\nResults: ")
     for filtered in logs:
@@ -17,6 +21,20 @@ with open(f"logs/{file}","r") as logs:
             match += 1
             found = True
             print(f"{match}. {filtered.strip()}")
+
+            #IP Counter
+
+            ip = re.search(r"\d+\.\d+\.\d+\.\d+", filtered)
+
+            if ip:
+                
+                ip_add = ip.group()
+
+                if ip_add in ip_num:
+                    ip_num[ip_add] += 1
+                else:
+                    ip_num[ip_add] = 1
+
     if not found:   
          print("No matched ")
 
@@ -27,3 +45,10 @@ print(f"Total events scanned: {num}")
 if num > 0 :
     percent = (match / num) * 100
     print(f"Match rate: {percent:.2f}%")
+
+print("\n------------Extracted IP------------\n")
+
+for ip, count in ip_num.items():
+    print(f"{ip} -> {count}")
+
+print("\n--------------------Done Executing--------------------")
